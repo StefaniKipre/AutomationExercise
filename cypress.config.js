625cc243c1cd4fc01bcc8b67158852fb3770e231
StefaniKipre <stefani.kiprevska@gmail.com>
2024-05-22 10:18:46 +0200

    added mochawesome reporter to project

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,18 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Automation Exercise Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     specPattern: "AutomationExercise/Tests.cy.js",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
